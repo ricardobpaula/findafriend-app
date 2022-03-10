7b0c9c966ae0ae7e5a35f939f03a78a243eac946
Ricardo de Paula <flashdepaula@gmail.com>
2022-03-10 19:05:14 -0300

    feat: adding styles to no result dashboard

--- src/components/NoResult.tsx
+++ src/components/NoResult.tsx
@@ -1,31 +1,46 @@
 import React from 'react'
 import {
-    StyleSheet,
-    Dimensions
+  Text,
+  View,
+  StyleSheet,
+  Dimensions
 } from 'react-native'
 
 import LottieView from 'lottie-react-native'
 
 import NotFoundAnimation from '../assets/lottie/no_results.json'
+import colors from '../styles/colors'
+import fonts from '../styles/fonts'
 
-// TODO estilizar no results
 const NoResult:React.FC = () => {
-    return (
-        <>
-            <LottieView 
+  return (
+        <View style={styles.container}>
+            <LottieView
                 source={NotFoundAnimation}
                 autoPlay
                 loop={true}
                 style={styles.gif}
             />
-        </>
-    )
+            <Text style={styles.text}> Não encontramos pets próximos a você =(</Text>
+        </View>
+  )
 }
 
 export default NoResult
 
 const styles = StyleSheet.create({
-    gif: {
-        width: Dimensions.get('window').width * 0.7
-    }
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  gif: {
+    width: Dimensions.get('window').width * 0.6
+  },
+  text: {
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    fontSize: 16,
+    padding: 10
+  }
 })
